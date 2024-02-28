9e80779f110d641d62baf20625729d1c202ccf5f
salimzayd <zidsalim786@gmail.com>
2024-02-28 18:24:02 +0530

    as

--- src/components/fetch.js
+++ src/components/fetch.js
@@ -1,24 +1,35 @@
-import React,{useState,useEffect} from "react";
+// import React,{useState,useEffect} from "react";
 
-function Fetching () {
-    const [data,setdata] = useState([])
+// function Fetching () {
+//     const [data,setdata] = useState([])
     
-    useEffect(() =>{
-        fetch('https://jsonplaceholder.typicode.com/todos/1')
-        .then(response => response.json())
-        .then(data => console.log(data))
-        .catch(console.error())
-    },[])
-    return
-    //     <div>
-    //     {/* Access specific properties of the 'data' object */}
-    //     <h1>User ID: {data.userId}</h1>
-    //     <p>ID: {data.id}</p>
-    //     <p>Title: {data.title}</p>
+//     useEffect(() =>{
+//         fetch('https://jsonplaceholder.typicode.com/todos/1')
+//         .then(response => response.json())
+//         .then(data => console.log(data))
+//         .catch(console.error())
+//     },[])
+//     return
+//     //     <div>
+//     //     {/* Access specific properties of the 'data' object */}
+//     //     <h1>User ID: {data.userId}</h1>
+//     //     <p>ID: {data.id}</p>
+//     //     <p>Title: {data.title}</p>
         
-    //   </div>
+//     //   </div>
 
-    // )
+//     // )
+// }
+
+// export default Fetching;
+
+
+
+async function datafetching(){
+    const API = "https://jsonplaceholder.typicode.com/todos/1"
+    const data  = await fetch(API);
+    const jsondata = await data.json();
+    console.log(jsondata)
 }
 
-export default Fetching;
+datafetching();
